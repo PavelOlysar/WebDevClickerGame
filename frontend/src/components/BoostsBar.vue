955b2933b79024fa3e2f970298f7c673b6c7050d
Pavel Olyšar <olysarp@gmail.com>
2025-02-18 17:36:06 +0100

    add boosts bar

---
+++ frontend/src/components/BoostsBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card bg-base-100 shadow-xl">
+    <div class="card-body">
+      <h2 class="card-title">Boosts</h2>
+      <div class="flex flex-col gap-2">
+        <button class="btn btn-outline gap-2">
+          <Icon icon="logos:html-5" />
+          Coffee
+        </button>
+        <button class="btn btn-outline gap-2">
+          <Icon icon="logos:css-3" />
+          Energy Drink
+        </button>
+        <button class="btn btn-outline gap-2">
+          <Icon icon="logos:javascript" />
+          Tea
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// import { Icon } from "@iconify/vue";
+</script>
